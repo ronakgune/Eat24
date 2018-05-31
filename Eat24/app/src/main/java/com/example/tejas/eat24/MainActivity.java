82793fb05ceb8efb06ad54e8fd80414f9bd97f7b
tejaswi1493 <38870039+tejaswi1493@users.noreply.github.com>
2018-05-30 23:47:30 -0700

    Revert "Add files via upload"
    This reverts commit 70f460e5967b8f0c1fff4d8720f25a86e7cefc5a.

--- Eat24/app/src/main/java/com/example/tejas/eat24/MainActivity.java
+++ Eat24/app/src/main/java/com/example/tejas/eat24/MainActivity.java
@@ -1,53 +1,50 @@
-package com.example.poojanshah.recyclerview;
+package com.example.tejas.eat24;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-
-    private List<ListItem> listItems;
-
-
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
 
-        listItems = new ArrayList<>();
+       // txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Nabila.ttf");
 
-        for(int i = 0; i<=10; i++){
-            ListItem listItem = new ListItem(
-                    "heading " + (i+1),
-
-                    "Some Text................."
-
-            );
-
-            listItems.add(listItem);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
+        });
 
-            adapter = new MyAdapter(listItems, this);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(MainActivity.this,SignUp.class);
+                startActivity(signUp);
+            }
+        });
 
-            recyclerView.setAdapter(adapter);
-
-
-
-
-        }
-
-
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+            }
+        });
     }
+}
