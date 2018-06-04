ccab81d84857ca97e264456ffd2129ba66f7c2e7
ronakgune <ronakgune@gmail.com>
2018-06-04 11:11:26 -0700

    v2.1
    Basic Sign Up and Sign In Feature added

--- Eat24/app/src/main/java/com/example/tejas/eat24/MainActivity.java
+++ Eat24/app/src/main/java/com/example/tejas/eat24/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.tejas.eat24;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignIn,btnSignUp;
+    EditText email, password;
 
 
     @Override
@@ -18,15 +29,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
         btnSignIn = (Button)findViewById(R.id.button_signin);
         btnSignUp = (Button)findViewById(R.id.button_signup);
+        email = findViewById(R.id.editEmail);
+        password = findViewById(R.id.editPass);
 
 
-        //Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Nabila.ttf");
+
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+               String email_val = email.getText().toString();
+               String pass_val = password.getText().toString();
+
+               if(TextUtils.isEmpty(email_val)){
+                   email.setError("Please Enter Email");
+               }
+               else if(TextUtils.isEmpty(pass_val)){
+                   password.setError("Please Enter Password");
+               }else{
+                   mAuth.signInWithEmailAndPassword(email_val, pass_val)
+                           .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                               @Override
+                               public void onComplete(@NonNull Task<AuthResult> task) {
+                                   if (task.isSuccessful()) {
+                                       // Sign in success, update UI with the signed-in user's information
+//                                       Log.d(TAG, "signInWithEmail:success");
+                                       Toast.makeText(MainActivity.this, "Authentication Successful",
+                                               Toast.LENGTH_SHORT).show();
+                                       FirebaseUser user = mAuth.getCurrentUser();
+//                                       updateUI(user);
+                                   } else {
+                                       // If sign in fails, display a message to the user.
+//                                       Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                       Toast.makeText(MainActivity.this, "Authentication failed.",
+                                               Toast.LENGTH_SHORT).show();
+//                                       updateUI(null);
+                                   }
+
+                                   // [START_EXCLUDE]
+                                   if (!task.isSuccessful()) {
+                                       Toast.makeText(MainActivity.this, "Authentication failed.",
+                                               Toast.LENGTH_SHORT).show();
+                                   }
+                                   // [END_EXCLUDE]
+                               }
+                           });
+               }
+
+
 
             }
         });
--- Eat24/app/src/main/java/com/example/tejas/eat24/SignUp.java
+++ Eat24/app/src/main/java/com/example/tejas/eat24/SignUp.java
@@ -1,86 +1,105 @@
 package com.example.tejas.eat24;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tejas.eat24.Model.User;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.rengwuxian.materialedittext.MaterialEditText;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SignUp extends AppCompatActivity {
 
     EditText edituser,editpass,editrepass,editemail;
     Button btn_signUp;
-
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        final FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
         edituser = (EditText) findViewById(R.id.edituser);
         editpass = (EditText) findViewById(R.id.editpass);
         editrepass = (EditText) findViewById(R.id.editrepass);
         editemail = (EditText) findViewById(R.id.editemail);
 
-        //edtName = (EditText) findViewById(R.id.edtName);
-        //edtPassword = (EditText) findViewById(R.id.edtPassword);
-        //edtPhone = (EditText) findViewById(R.id.edtPhone);
 
         btn_signUp = (Button)findViewById(R.id.btn_signUp);
 
-        //Init Firebase
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference table_user = database.getReference("User");
-
-
-
         btn_signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
 
-                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
-                mDialog.setMessage("Please wait...");
-                mDialog.show();
+                String pass = editpass.getText().toString();
+                String pass_ver = editrepass.getText().toString();
+                String email = editemail.getText().toString();
 
-                table_user.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        //Check if already user email registered
-                        if(dataSnapshot.child(editemail.getText().toString()).exists()) {
-                            mDialog.dismiss();
-                            Toast.makeText(SignUp.this,"Email already registered", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            mDialog.dismiss();
-                            User user = new User(edituser.getText().toString(),editpass.getText().toString());
-                            table_user.child(editemail.getText().toString()).setValue(user);
-                            Toast.makeText(SignUp.this,"SignUp Successful", Toast.LENGTH_SHORT).show();
-                            finish();
+                if(TextUtils.isEmpty(pass)){
+                    editpass.setError("Enter Password");
+                }
+                else if(TextUtils.isEmpty(pass_ver)){
+                    editrepass.setError("Enter Verify Password");
+                }
+                else if(TextUtils.isEmpty(email)){
+                    editemail.setError("Enter Email");
+                }
+                else{
+                    if(pass.equals(pass_ver)){
+//                    Toast.makeText(SignUp.this, "Equals", Toast.LENGTH_SHORT).show();
 
-                        }
+                        mAuth.createUserWithEmailAndPassword(email, pass)
+                                .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            // Sign in success, update UI with the signed-in user's information
+                                            //Log.d(TAG, "createUserWithEmail:success");
+                                            Toast.makeText(SignUp.this, "Authentication Successful",
+                                                    Toast.LENGTH_SHORT).show();
+                                            FirebaseUser user = mAuth.getCurrentUser();
+
+                                            //updateUI(user);
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+//                                            updateUI(null);
+                                        }
+
+                                        // [START_EXCLUDE]
+                                        //hideProgressDialog();
+                                        // [END_EXCLUDE]
+                                    }
+                                });
+
+                    }else {
+                        Toast.makeText(SignUp.this, "Passwords Don't Match", Toast.LENGTH_SHORT).show();
                     }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
+                }
 
 
             }
-
         });
+
+
     }
 }
